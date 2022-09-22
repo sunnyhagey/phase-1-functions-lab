50dbb26a5f243fc2ac1def44e1c8d62ca89801d8
Ragan Hagey <raganahagey@gmail.com>
2022-09-22 16:30:23 -0400

    commit tree a938d2ffbf63bc8199cd644f3d5ecac9cb2b4514

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickup) {
+    if (pickup > 42) {
+        return pickup - 42
+    } else {
+        return 42 - pickup
+    }
+}
+
+function distanceFromHqInFeet(pickup) {
+   return distanceFromHqInBlocks(pickup) * 264
+}
+
+ function distanceTravelledInFeet (start, finish) {
+    if (start < finish) {
+    return (finish - start) * 264}
+    else {return (start-finish) * 264}
+}
+
+
+function calculatesFarePrice(start, finish) {
+    const feet = distanceTravelledInFeet(start, finish)
+    if (feet < 400) {
+        return 0
+    }
+    else if (feet >=400 < 2000) {
+        return (feet-400) * 0.02
+    }
+    else if (feet >= 2000 < 2500) {
+        return 25
+    }
+    else if (feet >= 2500) {
+        return "cannot travel that far"
+    }
+}
